ae9d2439c3680ffe9d11334d0bfad549768e6945
Ken Tai <ken.tsk@gmail.comm>
2023-06-02 14:52:34 +0800

    config postgresql

--- tutorzon/settings.py
+++ tutorzon/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'tutorzon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tutorzon_db',
+        'USER': 'postgres',
+        'PASSWORD': '731220'
+        'HOST': 'localhost',
     }
 }
 

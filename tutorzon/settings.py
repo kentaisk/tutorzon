45596ad0d9ca5c7e6f3969ad7e1b42ef604a2a71
Ken Tai <ken.tsk@gmail.comm>
2023-06-04 00:09:32 +0800

    config team models

--- tutorzon/settings.py
+++ tutorzon/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'tutorzon_db',
         'USER': 'postgres',
-        'PASSWORD': '731220'
+        'PASSWORD': '731220',
         'HOST': 'localhost',
     }
 }
@@ -122,6 +123,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
+# Media settings
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
